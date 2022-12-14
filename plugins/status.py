bb8282fee065841f5d7121f134a9848043cd9ff2
KurimuzonAkuma <arturgayauskas@gmail.com>
2022-12-15 00:08:22 +0300

    commits since fork created

--- plugins/status.py
+++ plugins/status.py
@@ -7,7 +7,7 @@ import pyrogram
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from utils.misc import bot_uptime, modules_help, prefix, repo
+from utils.misc import bot_uptime, modules_help, prefix, repo, commits
 
 
 @Client.on_message(filters.command(["status", "статус"], prefix) & filters.me)
@@ -16,7 +16,10 @@ async def status(_, message: Message):
     text = "<b>===== Bot status =====</b>\n"
     text += f"<b>Prefix:</b> <code>{prefix}</code>\n"
     text += f"<b>Plugins:</b> <code>{len(modules_help)}</code>\n"
-    text += f"<b>Current version:</b> <code>{repo.head.commit.hexsha[:7]}</code>\n"
+    text += "<b>Current version:</b> "
+    text += f"<code>{repo.head.commit.hexsha[:7]} ({commits.get('current')})</code>\n"
+    text += f"<b>Latest version:</b> <code>{repo.remotes.origin.refs.master.commit.hexsha[:7]}"
+    text += f" ({commits.get('latest')})</code>\n"
     text += f"<b>Branch:</b> <code>{repo.active_branch}</code>\n"
     text += f"<b>Uptime:</b> <code>{datetime.timedelta(seconds=perf_counter() - bot_uptime)}</code>\n\n"
 
@@ -29,7 +32,7 @@ async def status(_, message: Message):
     system_uptime = subprocess.run(["uptime", "-p"], capture_output=True).stdout.decode().strip()
     text += f"<b>Uptime:</b> <code>{system_uptime}</code>\n\n"
     text += "<a href='https://github.com/KurimuzonAkuma/Kurimuzon-Userbot'>Kurimuzon-Userbot</a>"
-    await message.edit(text)
+    await message.edit(text, disable_web_page_preview=True)
 
 
 modules_help["status"] = {
--- utils/misc.py
+++ utils/misc.py
@@ -13,3 +13,15 @@ bot_uptime = perf_counter()
 prefix = db.get("core.main", "prefix", default=".")
 
 repo = git.Repo(script_path)
+# 05c3cfe is the first commit of the fork
+commits = {
+    "latest": (
+        len(
+            list(
+                repo.iter_commits(f"05c3cfe..{repo.remotes.origin.refs.master.commit.hexsha[:7]}")
+            )
+        )
+        + 1
+    ),
+    "current": len(list(repo.iter_commits(f"05c3cfe..{repo.head.commit.hexsha[:7]}"))) + 1,
+}
