1daf9034e47e4e1bc42d5fdbd00068235a19e4b3
KurimuzonAkuma <arturgayauskas@gmail.com>
2023-03-30 15:51:04 +0300

    update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,10 @@
+import asyncio
 import json
 import sqlite3
 import threading
 
-from utils import config
+import aiosqlite
+from utils.config import db_name
 
 
 class Database:
@@ -30,40 +32,36 @@ class SqliteDatabase(Database):
         self._lock = threading.Lock()
 
     @staticmethod
-    def _parse_row(row: sqlite3.Row):
-        if row["type"] == "bool":
-            return row["val"] == "1"
-        elif row["type"] == "int":
-            return int(row["val"])
-        elif row["type"] == "str":
-            return row["val"]
-        else:
-            return json.loads(row["val"])
+    def _parse_row(self, row: sqlite3.Row):
+        parse_func = {"bool": lambda x: x == "1",
+                      "int": int,
+                      "str": lambda x: x,
+                      "json": json.loads}
+        return parse_func[row["type"]](row["val"])
 
-    def _execute(self, module: str, *args, **kwargs) -> sqlite3.Cursor:
-        self._lock.acquire()
-        try:
-            return self._cursor.execute(*args, **kwargs)
-        except sqlite3.OperationalError as e:
-            if str(e).startswith("no such table"):
-                sql = f"""
-                CREATE TABLE IF NOT EXISTS '{module}' (
-                var TEXT UNIQUE NOT NULL,
-                val TEXT NOT NULL,
-                type TEXT NOT NULL
-                )
-                """
-                self._cursor.execute(sql)
-                self._conn.commit()
-                return self._cursor.execute(*args, **kwargs)
-            raise e from None
-        finally:
-            self._lock.release()
+    def _create_table(self, module: str):
+        sql = f"""
+        CREATE TABLE IF NOT EXISTS '{module}' (
+        var TEXT UNIQUE NOT NULL,
+        val TEXT NOT NULL,
+        type TEXT NOT NULL
+        )
+        """
+        self._cursor.execute(sql)
+        self._conn.commit()
+
+    def _execute(self, module: str, sql, params=None):
+        with self._lock:
+            try:
+                return self._cursor.execute(sql, params)
+            except sqlite3.OperationalError as e:
+                if str(e).startswith("no such table"):
+                    self._create_table(module)
+                    return self._cursor.execute(sql, params)
+                raise e from None
 
     def get(self, module: str, variable: str, default=None):
-        sql = f"SELECT * FROM '{module}' WHERE var=:var"
-        cur = self._execute(module, sql, {"tabl": module, "var": variable})
-
+        cur = self._execute(module, f"SELECT * FROM '{module}' WHERE var=:var", {"var": variable})
         row = cur.fetchone()
         return default if row is None else self._parse_row(row)
 
@@ -108,4 +106,105 @@ class SqliteDatabase(Database):
         self._conn.close()
 
 
-db = SqliteDatabase(config.db_name)
+class AioSqliteDatabase(Database):
+    def __init__(self, file=db_name):
+        self._file = file
+        self._conn = None
+
+    async def _connect(self):
+        self._conn = await aiosqlite.connect(self._file)
+        self._conn.row_factory = aiosqlite.Row
+        self._cursor = await self._conn.cursor()
+        self._lock = asyncio.Lock()
+
+    @staticmethod
+    def _parse_row(self, row: aiosqlite.Row):
+        parse_func = {"bool": lambda x: x == "1",
+                      "int": int,
+                      "str": lambda x: x,
+                      "json": json.loads}
+        return parse_func[row["type"]](row["val"])
+
+    async def _create_table(self, module: str):
+        sql = f"""
+        CREATE TABLE IF NOT EXISTS '{module}' (
+        var TEXT UNIQUE NOT NULL,
+        val TEXT NOT NULL,
+        type TEXT NOT NULL
+        )
+        """
+        self._cursor.execute(sql)
+        self._conn.commit()
+
+    async def _execute(self, module: str, *args, **kwargs) -> aiosqlite.Cursor:
+        try:
+            return await self._cursor.execute(*args, **kwargs)
+        except aiosqlite.OperationalError as e:
+            if str(e).startswith("no such table"):
+                await self._create_table(module)
+                return await self._cursor.execute(*args, **kwargs)
+            raise e from None
+
+    async def get(self, module: str, variable: str, default=None):
+        if not self._conn:
+            await self._connect()
+        sql = f"SELECT * FROM '{module}' WHERE var=:var"
+        cur = await self._execute(module, sql, {"var": variable})
+
+        row = await cur.fetchone()
+        result = default if row is None else self._parse_row(row)
+        await self.close()
+        return result
+
+    async def set(self, module: str, variable: str, value) -> bool:
+        if not self._conn:
+            await self._connect()
+        sql = f"""
+        INSERT INTO '{module}' VALUES ( :var, :val, :type )
+        ON CONFLICT (var) DO
+        UPDATE SET val=:val, type=:type WHERE var=:var
+        """
+
+        if isinstance(value, bool):
+            val = "1" if value else "0"
+            typ = "bool"
+        elif isinstance(value, str):
+            val = value
+            typ = "str"
+        elif isinstance(value, int):
+            val = str(value)
+            typ = "int"
+        else:
+            val = json.dumps(value)
+            typ = "json"
+
+        await self._execute(module, sql, {"var": variable, "val": val, "type": typ})
+        await self._conn.commit()
+        await self.close()
+        return True
+
+    async def remove(self, module: str, variable: str):
+        if not self._conn:
+            await self._connect()
+        sql = f"DELETE FROM '{module}' WHERE var=:var"
+        await self._execute(module, sql, {"var": variable})
+        await self._conn.commit()
+        await self.close()
+
+    async def get_collection(self, module: str) -> dict:
+        if not self._conn:
+            await self._connect()
+        sql = f"SELECT * FROM '{module}'"
+        cur = await self._execute(module, sql)
+        result = {row["var"]: self._parse_row(row) async for row in cur}
+        await self.close()
+        return result
+
+    async def close(self):
+        await self._conn.commit()
+        await self._cursor.close()
+        await self._conn.close()
+        self._conn = None
+
+
+db = SqliteDatabase(db_name)
