96b9cd4a8d70290cdf4bc5c0d59e7116c47af930
KurimuzonAkuma <arturgayauskas@gmail.com>
2023-05-09 00:30:00 +0300

    New logging format

--- utils/scripts.py
+++ utils/scripts.py
@@ -17,27 +17,33 @@ from utils.misc import modules_help, script_path
 
 
 class CustomFormatter(logging.Formatter):
-    grey = "\x1b[38;21m"
-    blue = "\x1b[38;5;39m"
-    yellow = "\x1b[38;5;226m"
-    red = "\x1b[38;5;196m"
-    bold_red = "\x1b[31;1m"
+    # Colors
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    # Styles
     reset = "\x1b[0m"
+    bold = "\x1b[1m"
 
-    def __init__(self, fmt):
-        super().__init__()
-        self.fmt = fmt
-        self.FORMATS = {
-            logging.DEBUG: self.grey + self.fmt + self.reset,
-            logging.INFO: self.blue + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.bold_red + self.fmt + self.reset,
-        }
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
 
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
+        log_color = self.COLORS[record.levelno]
+        fmt = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        fmt = fmt.replace("(black)", self.black + self.bold)
+        fmt = fmt.replace("(reset)", self.reset)
+        fmt = fmt.replace("(levelcolor)", log_color)
+        fmt = fmt.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(fmt, "%Y-%m-%d %H:%M:%S", style="{")
         return formatter.format(record)
 
 
