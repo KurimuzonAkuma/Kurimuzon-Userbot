0b3fcb7497ece525a175362d688584b6c7c29927
KurimuzonAkuma <arturgayauskas@gmail.com>
2023-08-04 00:25:41 +0300

    Update stories.py

--- plugins/stories.py
+++ plugins/stories.py
@@ -1,58 +1,55 @@
-import random
-
 from pyrogram import Client, filters
-from pyrogram.raw.functions.messages import GetMessages
-from pyrogram.raw.functions.stories import SendStory
-from pyrogram.raw.types import (
-    InputMediaPhoto,
-    InputMediaUploadedDocument,
-    InputMessageID,
-    InputPhoto,
-    InputPrivacyValueAllowAll,
-)
+from pyrogram.raw.types import InputPrivacyValueAllowAll
 from pyrogram.types import Message
 
 from utils.filters import command
 from utils.misc import modules_help
-from utils.scripts import with_reply
+from utils.scripts import get_args
 
 
 @Client.on_message(command(["story"]) & filters.me & ~filters.forwarded & ~filters.scheduled)
-@with_reply
 async def post_story(client: Client, message: Message):
-    if message.reply_to_message.media.value not in ["photo", "video", "animation"]:
-        return await message.edit_text("Only photos and videos and GIFs are supported!")
+    media = (
+        message.video
+        or message.photo
+        or (
+            message.reply_to_message
+            and (message.reply_to_message.video or message.reply_to_message.photo)
+        )
+    )
+    if not media:
+        return await message.edit_text("<b>No media found!</b>")
 
-    if message.reply_to_message.photo:
-        msg = await client.invoke(GetMessages(id=[InputMessageID(id=message.reply_to_message.id)]))
-        photo = msg.messages[0].media.photo
-        media = InputMediaPhoto(
-            id=InputPhoto(
-                id=photo.id,
-                access_hash=photo.access_hash,
-                file_reference=photo.file_reference,
-            )
+    if (message.media and message.media.value not in ["photo", "video", "animation"]) or (
+        message.reply_to_message.media
+        and message.reply_to_message.media.value not in ["photo", "video", "animation"]
+    ):
+        return await message.edit_text("<b>Only photos and videos and GIFs are supported!<b>")
+
+    args, nargs = get_args(message=message, use_reply=True)
+
+    if "-c" in args:
+        caption = message.caption or (
+            message.reply_to_message and message.reply_to_message.caption
         )
     else:
-        media = InputMediaUploadedDocument(
-            file=await client.save_file(
-                path=await message.reply_to_message.download(in_memory=True)
-            ),
-            mime_type="video/mp4",
-            attributes=[],
-        )
+        caption = " ".join(args)
 
-    await client.invoke(
-        SendStory(
-            media=media,
-            privacy_rules=[InputPrivacyValueAllowAll()],
-            random_id=random.randint(-1 << 31 - 1, 1 << 31 - 1),
-            period=86400,
-        )
+    media = (
+        await message.download(in_memory=True)
+        if message.media
+        else await message.reply_to_message.download(in_memory=True)
+    )
+    await client.send_story(
+        media=media, caption=caption, privacy_rules=[InputPrivacyValueAllowAll()], period=86400 * 7
     )
 
     await message.edit_text("Story posted!")
 
 
 module = modules_help.add_module("stories", __file__)
-module.add_command("story", "Post story from reply", "[reply]")
+module.add_command(
+    "story",
+    "Post story from message or reply. Pass the '-c' argument to take the caption from the media, otherwise the message will be used",
+    "[reply] [-c]",
+)
